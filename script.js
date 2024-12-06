8819566e9745aab8b3659f5bf0215e3ad38701cf
Juan San Emeterio <juanjser@gmail.com>
2024-12-06 15:21:23 -0500

    Added the shopify page link

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 document.getElementById('year').textContent = new Date().getFullYear();
-
-const menuToggle = document.getElementById('menu-toggle');
-const navList = document.getElementById('nav-list');
-
-menuToggle.addEventListener('click', () => {
-  navList.style.display = navList.style.display === 'flex' ? 'none' : 'flex';
-});
